93c51a8f053107bbf67f9bc53f1b3197a66e73e9
Chaaampy <tony.evariste@gmail.com>
2018-02-05 17:16:12 +0100

    Add files via upload

--- client/views/application/project.js
+++ client/views/application/project.js
@@ -42,22 +42,26 @@ Template.project.onRendered(function(){
             
             $('.results-global').append('<table><tr><td><span>Global results</span></td><td><span class="errors">Errors : </span>' + totalErrors + '</td><td><span class="warnings">Warnings : </span>' + totalWarnings + '</td></tr></table>');
         
-            //for(var nb in data.files) {
-            //    console.log(nb, data.files[nb]);
-            //}
-            //console.log(JSON.stringify(data.files[0]));
-            //console.log(data.files);
-            //console.log(data.files['/tmp/1/processus.php']);
-
-            for (var test in data.files) {
-              console.log(data.files[test].messages[0]);  
-            };
-            
-            //console.log(data.files);
-        
             $('.results-details').append('<table><tr><td><span>Details</span></td><td><span class="strong">Files : </span>' + nbFiles + '</td><td><a href="javascript:void(0) class="link">See more</a></td></tr></table>');
             
-            $('.results-full').append('<table><thead><tr><td><span class="strong">File</span></td><td><span class="strong">Line</td><td><span class="strong">Type</span></td><td><span class="strong">Message</span></td></tr></thead></table>');
+            $('.results-full').append('<table><thead><tr><td><span class="strong">File</span></td><td><span class="strong">Line</td><td><span class="strong">Type</span></td><td><span class="strong">Message</span></td></tr></thead><tbody></tbody></table>');
+            
+            for (var test in data.files) {
+                try {
+                  if ((Object.keys(data.files[test].messages[0]).length) === '') {
+                        return null;
+                    } else {
+                        let errorFile = test;
+                        let errorLine = (data.files[test].messages[0].line);
+                        let errorType = (data.files[test].messages[0].type);
+                        let errorMessage = (data.files[test].messages[0].message);
+                        $('.results-full table tbody').append('<tr cellspacing="5"><td>' + test + '</td><td>' + errorLine + '</td><td>' + errorType + '</td><td>' + errorMessage + '</td></tr>')
+                    }
+                }
+                catch(error) {
+                  // errors here
+                }
+            };
       }); 
     });
 });
