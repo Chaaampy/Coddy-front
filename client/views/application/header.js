a5cf2b1d727e3cb71ac54e993c824a66860ca105
Chaaampy <tony.evariste@gmail.com>
2018-02-07 15:23:38 +0100

    Add files via upload

--- client/views/application/header.js
+++ client/views/application/header.js
@@ -6,3 +6,12 @@ Template.header.events({
         });
 	}
 });
+
+Template.header.events({    
+    'click .nav-mobile a': function(){
+        $(function() {
+              $('.nav-mobile').toggleClass('nav-mobile-active');
+              $('.trigger-wrapper').toggleClass('is-active');
+        });
+	}
+});
--- client/views/application/project.js
+++ client/views/application/project.js
@@ -10,13 +10,16 @@ Template.project.onRendered(function(){
         $('.step-two-form .btn-primary').click(function() {
             let repo = $('#project-select').val();
             let mail = $('#project-mail').val();
-            
-            if (mail != '') {
+            let pattern = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+                        
+            if (pattern.test(mail)) {
                 $('.step-two-form, .step-two-text').fadeOut(time);
                 $('.step-three-form, .step-three-text').fadeIn(time);
                 $('.step-progress span:nth-child(3)').toggleClass('step-active step-inactive');
                 $('#project-repo-final').val(repo);
                 $('#project-mail-final').val(mail);
+            } else {
+                alert('This is not a valid e-mail address');
             }
         });
         
@@ -35,10 +38,12 @@ Template.project.onRendered(function(){
     });
     
     $(function() {
-        $.getJSON('/code-sniffer-result.json',function(data){
-            let nbFiles = (Object.keys(data.files).length);
-            let totalErrors = (data.totals.errors);
-            let totalWarnings = (data.totals.warnings);
+        $.getJSON('/test.json',function(data){
+            let dataPath = (data.content.sniffer);
+            let nbFiles = (Object.keys(dataPath.files).length);
+            console.log(nbFiles);
+            let totalErrors = (dataPath.totals.errors);
+            let totalWarnings = (dataPath.totals.warnings);
             
             $('.results-global').append('<table><tr><td><span>Global results</span></td><td><span class="errors">Errors : </span>' + totalErrors + '</td><td><span class="warnings">Warnings : </span>' + totalWarnings + '</td></tr></table>');
         
@@ -46,17 +51,17 @@ Template.project.onRendered(function(){
             
             $('.results-full').append('<table><thead><tr><td><span class="strong">File</span></td><td><span class="strong">Line</td><td><span class="strong">Type</span></td><td><span class="strong">Message</span></td></tr></thead><tbody></tbody></table>');
         
-            for (var loop in data.files) {
+            for (var loop in dataPath.files) {
                 try {
-                    if ((Object.keys(data.files[loop].messages[0]).length) === '') {
+                    if ((Object.keys(dataPath.files[loop].messages[0]).length) === '') {
                         return null;
                     } else {
-                        for (var i = 0; i < (data.files[loop].messages.length); i++) {
+                        for (var i = 0; i < (dataPath.files[loop].messages.length); i++) {
                         let errorFile = loop;
-                        let errorLine = (data.files[loop].messages[i].line);
-                        let errorType = (data.files[loop].messages[i].type);
-                        let errorMessage = (data.files[loop].messages[i].message);
-                        $('.results-full table tbody').append('<tr><td><a target="_blank" class="link" href="' + loop + '#L' + errorLine + '">' + loop + '</a></td><td>' + errorLine + '</td><td class="' + errorType + '">' + errorType + '</td><td>' + errorMessage + '</td></tr>');
+                        let errorLine = (dataPath.files[loop].messages[i].line);
+                        let errorType = (dataPath.files[loop].messages[i].type);
+                        let errorMessage = (dataPath.files[loop].messages[i].message);
+                        $('.results-full table tbody').append('<tr><td><a target="_blank" class="link" href="' + data.content_url + '' + loop + '#L' + errorLine + '">' + loop + '</a></td><td>' + errorLine + '</td><td class="' + errorType + '">' + errorType + '</td><td>' + errorMessage + '</td></tr>');
                         }
                     }
                 }
